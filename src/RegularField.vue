ecf96fb0ffba7083989bd74c94ad27284efd9642
Hèctor <thehector1593@gmail.com>
2021-10-13 15:08:53 +0200

    WIP: environment setup

--- src/RegularField.vue
+++ src/RegularField.vue
@@ -1,58 +1,44 @@
+<script lang="ts">
+export default {
+  name: "RegularField",
+  props: {
+    error: String,
+    max: Number,
+
+    type: {
+      type: String,
+      default: "text",
+    }
+  },
+
+  components: {},
+
+  data () {
+      return {
+          content: "",
+          visible: false,
+      }
+  },
+
+  watch: {
+    
+  },
+
+  computed: {
+
+  },
+
+  methods: { 
+    
+  }
+}
+</script>
+
 <template>
-<div class="regular-field rcorner"
-    :class="{error: error, expanded: value}">
-    <div id="placeholder"
-        v-if="placeholder">
-        <text :class="{error: error}">{{placeholder}}</text>
-    </div>
-
-    <input  ref="field"
-            v-model="value"
-            :maxlength="max"
-            :type="type === 'password' && show? 'text' : type">
-
-    <button v-if="type === 'password'"
-            class="flex"
-            :class="{expanded: value,
-                    show: show}"
-            v-on:click="onClick">
-        {{ show? '&ndash;' : '&#x25E6;'}}
-    </button>
-</div>
-<!--p  class="box" 
-    :class="{visible: error}">{{error}}
-</p-->
+<h1> Hello world </h1>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-@import "fibonacci-styles";
+
 </style>
-
-<script lang="ts">
-import Vue from "vue"
-import Component from 'vue-class-component'
-import { Watch } from 'vue-property-decorator'
-
-@Component({  
-  components: { },  
-  props: {
-      id: String,
-      error: String,
-      type: String,
-      max: Number,
-  },  
-})  
-export default class RegularField extends Vue {  
-    content: string = ""
-    visible: boolean = false
-
-    @Watch("content")
-    public onContentChanged(content: string): void {
-        console.log(content)
-    }
-
-    created () { }
-    mounted () { }
-}
-</script>
