b81e8e8da92ce8ef8929b7f1559e29843d67fb61
HectorMRC <thehector1593@gmail.com>
2021-12-27 23:25:12 +0100

    package updates

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
-import RegularField from "./RegularField.vue";
-import DiscretField from "./DiscretField.vue";
+import RegularField from "./RegularField.vue"
+import DiscretField from "./DiscretField.vue"
+import { defineCustomElement } from "vue"
+
+const REGULAR_FIELD_TAG = "regular-field"
+const DISCRET_FIELD_TAG = "discret-field"
 
 export {
     RegularField,
     DiscretField
 }
 
-import { createApp } from 'vue'
-import Demo from "./demo.vue"
+export function include(app: any): any {
+    app.component(REGULAR_FIELD_TAG, RegularField)
+       .component(DISCRET_FIELD_TAG, DiscretField)
+}
 
-createApp(Demo).mount('#app')
+// Remember to format all SFC names as *.ce.vue to include its styles into the web component
+// see: https://v3.vuejs.org/guide/web-components.html
+
+const ElementRegularField = defineCustomElement(RegularField)
+const ElementDiscretField = defineCustomElement(DiscretField)
+
+
+export {
+    ElementRegularField,
+    ElementDiscretField,
+};
+
+
+export function register() {
+    customElements.define(REGULAR_FIELD_TAG, ElementRegularField)
+    customElements.define(DISCRET_FIELD_TAG, ElementDiscretField)
+}
