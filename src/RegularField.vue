09fc97c7c31b1a3d301c100e19b44736f71d14da
HectorMRC <thehector1593@gmail.com>
2021-12-28 15:12:57 +0100

    error slot added

--- src/RegularField.vue
+++ src/RegularField.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="regular-field"
       :class="{active: content.length}">
-    <div class="input-container">
-      <label v-if="placeholder"> {{placeholder}} </label>
-      <input red="entry" v-model="content"/>
+    <div class="input-container"
+        :class="{error: hasError}">
+      <slot>
+        <label v-if="placeholder"
+               @click="focus"> {{placeholder}} </label>
+        <input ref="entry" v-model="content"/>
+      </slot>
+    </div>
+    <div class="error-container">
+      <slot name="error" :error="error">
+        <span v-if="hasError">{{error}}</span>
+      </slot>
     </div>
   </div>
 </template>
@@ -39,6 +48,10 @@ export default defineComponent({
   },
 
   computed: {
+    hasError(): boolean {
+      return this.error != undefined && this.error.length > 0
+    },
+
     showButton(): boolean {
       return this.type === PWD_INPUT_TYPE && this.content.length > 0
     },
@@ -53,18 +66,10 @@ export default defineComponent({
   },
 
   methods: { 
-    setInputFocus(focus: boolean) {
-      this.focused = focus
+    focus() {
+      let entryRef: any = this.$refs.entry
+      entryRef.focus()
     },
-
-    onActionClicked() {
-      // if (this.hasAction) {
-      //   this.$emit(CLICK_EVENT_NAME, this.content)
-      //   return
-      // }
-
-      this.visible = !this.visible
-    }
   },
 
 })
@@ -86,24 +91,38 @@ export default defineComponent({
     }
 
     label {
-      font-size: $fib-6 * 1px;
+      font-size: $small-font-size;
     }
   }
 
   .input-container {
     position: relative;
-    padding-left: $margin-bounds;
-    padding-right: $margin-bounds;
+    overflow: hidden;
 
     label, input {
       position: absolute;
-      width: 100%;
       height: $default-height;
       line-height: $default-height;
+      padding-left: $margin-bounds;
+    }
+
+    label {
+      cursor:text;
+    }
+  }
+
+  .error-container {
+    padding-left: $margin-bounds;
+    color: $error-color;
+
+    span {
+      font-size: $small-font-size;
+      font-weight: 600;
     }
   }
 
   input {
+    width: 100%;
     bottom: 0px;
     font-size: 1em;
     z-index: 1;
