cce9724b47efdef45f31c3d70a232048dbbecccf
HectorMRC <thehector1593@gmail.com>
2021-12-31 08:58:00 +0100

    regular button styles are no longer scoped because of consistency with slot's content

--- src/RegularField.vue
+++ src/RegularField.vue
@@ -94,8 +94,7 @@ export default defineComponent({
 })
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style lang="scss">
 @import "global.scss";
 
 .regular-field {
